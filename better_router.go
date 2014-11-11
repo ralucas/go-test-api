5028a5c6b6dfc87a33008e1f0bbf239ae7737011
R.A. Lucas <richard@richardalucas.com>
2014-11-10 22:51:13 -0700

    initial commit

---
+++ better_router.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "html"
+  "log"
+  "net/http"
+  "time"
+  "encoding/json"
+
+  "github.com/gorilla/mux"
+)
+
+func main() {
+
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", Index)
+  router.HandleFunc("/todos", TodoIndex)
+  router.HandleFunc("/todos/{todoId}", TodoShow)
+
+  log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+  todos := Todos {
+    Todo{Name: "Write presentation"},
+    Todo{Name: "Host meetup"},
+  }
+
+  json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  todoId := vars["todoId"]
+  fmt.Fprintf(w, "Todo show: %s\n", todoId)
+}
+
+type Todo struct {
+  Name        string    `json:"name"`
+  Completed   bool      `json:"completed"`
+  Due         time.Time `json:"due"`
+}
+
+type Todos []Todo
+
