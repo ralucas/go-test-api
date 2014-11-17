358088549fe3d5c03c8df74d914a381ee3c011fb
R.A. Lucas <richard@richardalucas.com>
2014-11-16 23:18:38 -0700

    updated with modules

--- routes.go
+++ routes.go
@@ -36,16 +36,3 @@ var routes = Routes{
   },
 }
 
-func NewRouter() *mux.Router {
-
-  router := mux.NewRouter().StrictSlash(true)
-  for _, route := range routes {
-    router.
-      Methods(route.Method).
-      Path(route.Pattern).
-      Name(route.Name).
-      Handler(route.HandlerFunc)
-  }
-
-  return router
-}
